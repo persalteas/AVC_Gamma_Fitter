8fc82f7be358391da0baf66dd315e0830e60ba3d
Louis Becquey <louis.becquey@insa-lyon.fr>
2017-12-05 11:48:04 +0100

    correction noms de fichiers masques

--- projet1.m
+++ projet1.m
@@ -14,15 +14,15 @@ input_file = [ image_folder 'ConcentrationImage_patient_170.nii' ];
 pwi_170 = load_untouch_nii(input_file);
 
 
-input_file = [ mask_folder 'ConcentrationImage_patient_048.nii' ];
+input_file = [ mask_folder 'mask_patient_048.nii' ];
 mask_048 = load_untouch_nii(input_file);
 
-input_file = [ mask_folder 'ConcentrationImage_patient_249.nii' ];
+input_file = [ mask_folder 'mask_patient_249.nii' ];
 mask_249 = load_untouch_nii(input_file);
 
-input_file = [ mask_folder 'ConcentrationImage_patient_260.nii' ];
+input_file = [ mask_folder 'mask_patient_260.nii' ];
 mask_260 = load_untouch_nii(input_file);
 
-input_file = [ mask_folder 'ConcentrationImage_patient_170.nii' ];
+input_file = [ mask_folder 'mask_patient_170.nii' ];
 mask_170 = load_untouch_nii(input_file);
 
